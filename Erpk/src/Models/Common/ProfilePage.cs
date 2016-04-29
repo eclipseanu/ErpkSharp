89a70c82d9361b9b2c860bf07b3d80ff6b77f1a7
stil <p@ryciuk.com>
2016-04-29 14:53:34 +0200

    Add Community module

---
+++ Erpk/src/Models/Common/ProfilePage.cs
@@ -0,0 +1,21 @@
+﻿using Erpk.Http;
+
+namespace Erpk.Models.Common
+{
+    public class ProfilePage : FullPage
+    {
+        public ProfilePage(Response response, int citizenId)
+            : base(response)
+        {
+            var xs = response.XPath();
+
+            LookedUpCitizenId = citizenId;
+            LookedUpCitizenName = xs.Find("//img[@class='citizen_avatar'][1]").Extract();
+            AlreadyFriends = xs.FindAny("//a[@class='action_friend_remove tip']");
+        }
+
+        public int LookedUpCitizenId { get; }
+        public string LookedUpCitizenName { get; }
+        public bool AlreadyFriends { get; }
+    }
+}
---
+++ Erpk/src/Modules/CommunityModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Erpk.Http;
+using Erpk.Models.Common;
+
+namespace Erpk.Modules
+{
+    public class CommunityModule : Module
+    {
+        /// <summary>
+        ///     Invites citizen to your friends.
+        /// </summary>
+        public async Task<bool> InviteFriend(int citizenId)
+        {
+            var url = new Uri(Client.BaseUri, $"citizen/friends/add/{citizenId}?_token={Client.Session.Token}");
+            var req = Client.Post(url.ToString());
+            req.AddReferer($"citizen/profile/{citizenId}");
+
+            var res = await req.Send();
+            return res.Content.ReadAsStringAsync().Result.Contains("Your friendship request has been sent.");
+        }
+
+        /// <summary>
+        ///     Returns citizen profile page.
+        /// </summary>
+        public async Task<ProfilePage> GetProfile(int citizenId)
+        {
+            var res = await Client.Get("citizen/profile/" + citizenId).Send();
+            if (res.StatusCode == HttpStatusCode.NotFound)
+            {
+                throw new ArgumentOutOfRangeException(nameof(citizenId),
+                    "Citizen with ID " + citizenId + " does not exist.");
+            }
+
+            return new ProfilePage(res, citizenId);
+        }
+    }
+}
